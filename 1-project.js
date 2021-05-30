d883f339f65cdc2c92c03273c56ddca47a88ee9f
MohitJindal99 <mohitjd1999@gmail.com>
2021-05-30 17:09:17 +0530

    v1

---
+++ 1-project.js
@@ -0,0 +1,6 @@
+console.log("I am JavaScript");
+
+function hello() {
+    console.log("hello world");
+    console.log("Good Morning!");
+}
---
+++ 2-project.js
@@ -0,0 +1,20 @@
+console.log("2-project.JS");
+
+function changeColor(){
+    console.log("I will change the color!!");
+
+    let refId2 = document.querySelector("#id2");
+        refId2.style.color = "white";
+        refId2.style.backgroundColor = "tomato";
+}
+
+function changeRandomColor(){
+    let r = Math.floor(Math.random() * 255);
+    let g = Math.floor(Math.random() * 255);
+    let b = Math.floor(Math.random() * 255);
+
+    let colorCode = `rgb(${r},${g},${b})`;
+
+    let refId3 = document.querySelector("#id3");
+        refId3.style.color = colorCode;
+}
